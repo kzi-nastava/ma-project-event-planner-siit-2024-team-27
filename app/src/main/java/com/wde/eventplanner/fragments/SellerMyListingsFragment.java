6499b42a0efe588b126ed49b8cafb24632c964cb
Luka <79410671+lukascobbler@users.noreply.github.com>
2024-11-15 16:41:09 +0100

    fix: adding create service navigation

--- app/src/main/java/com/wde/eventplanner/fragments/SellerMyListingsFragment.java
+++ app/src/main/java/com/wde/eventplanner/fragments/SellerMyListingsFragment.java
@@ -5,12 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.button.MaterialButton;
 import com.wde.eventplanner.R;
 import com.wde.eventplanner.adapters.EventAdapter;
 import com.wde.eventplanner.adapters.ListingAdapter;
@@ -21,9 +25,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SellerMyListingsFragment extends Fragment {
+
+    private MaterialButton createServiceButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_seller_my_listings_screen, container, false);
+
+        createServiceButton = view.findViewById(R.id.createServiceButton);
+        createServiceButton.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(v);
+
+            // Navigate to RegistrationFragment
+            navController.navigate(R.id.SellerCreateServiceFragment);
+        });
 
         Spinner spinner = view.findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, new String[]{"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"});
