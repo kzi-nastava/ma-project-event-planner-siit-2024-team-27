1e9d3e8189a22f3ce4d2a60f61e33daaf7024759
Stankovic Aleksandar <stankovic.sv25.2022@uns.ac.rs>
2024-11-17 19:48:18 +0100

    polished source code

--- app/src/main/java/com/wde/eventplanner/constants/RegexConstants.java
+++ app/src/main/java/com/wde/eventplanner/constants/RegexConstants.java
@@ -8,7 +8,7 @@ public class RegexConstants {
     public static final String NAME_REGEX = "^[A-Za-z ]{1,50}$";
     public static final String CITY_REGEX = "^[A-Za-z ]{1,50}$";
     public static final String ADDRESS_REGEX = "^[A-Za-z0-9 ,.-]{5,100}$"; // allows alphanumeric characters and common address symbols
-    public static final String PHONE_REGEX = "^(\\+\\d{1,2}\\s?)?\\(?\\d{3}\\)?[-\\s]?\\d{3}[-\\s]?\\d{4}$";
+    public static final String PHONE_REGEX = "^\\+?\\d+$";
 
     public static boolean isStrongPassword(String password) {
         if (password.length() < MIN_PASSWORD_LENGTH) return false;
