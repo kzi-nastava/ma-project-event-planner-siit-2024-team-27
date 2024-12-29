a6732113ebb765e3e7a2862fb7c4c295e5d252a1
Momir Stanišić <momir.stanisic.ns@gmail.com>
2024-12-29 18:15:10 +0100

    fix: CustomViewPager height when switching tabs

--- app/src/main/java/com/wde/eventplanner/components/CustomViewPager.java
+++ app/src/main/java/com/wde/eventplanner/components/CustomViewPager.java
@@ -11,20 +11,39 @@ import androidx.viewpager.widget.ViewPager;
 public class CustomViewPager extends ViewPager {
     public CustomViewPager(@NonNull Context context) {
         super(context);
+        addOnPageChangeListener(new CustomOnPageChangedListener());
     }
 
     public CustomViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        addOnPageChangeListener(new CustomOnPageChangedListener());
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int height = 0;
+
         for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
             child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
             height = Math.max(height, child.getMeasuredHeight());
         }
+
         super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY));
+    }
+
+    private class CustomOnPageChangedListener implements OnPageChangeListener {
+        @Override
+        public void onPageSelected(int position) {
+            requestLayout();
+        }
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
     }
 }
